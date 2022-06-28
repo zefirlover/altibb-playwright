ae8c007ad072032d74c3438ff55021fcc2613ae6
zefirlover <ibodikmushta@gmail.com>
2022-06-28 16:44:09 +0300

    Fix import pt.4

--- tests/atrialFibrillationVideoPage.spec.ts
+++ tests/atrialFibrillationVideoPage.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { MedicalVideosPage } from "../pages/MedicalVideos.page";
-import { AtrialFibrillationVideoPage } from '../../altibb-playwright/pages/AtrialFibrillationVideo.page';
+import { AtrialFibrillationVideoPage } from "../pages/AtrialFibrillationVideo.page";
 const arrMedias = [ 'facebook', 'linkedin', 'twitter', 'instagram', 'whatsapp' ];
 
 test.describe('atrial fibrillation video page testing', () => {
